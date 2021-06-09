e7e61bc7d28c39b3f245d602cf57a6cc3bb834a0
Max <maxmorgangiles@gmail.com>
2021-06-09 15:05:26 +0100

    contact model + config and lib folders

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import Contact from './models/contact.js'
+import { port } from './config/environment.js'
+import connectToDatabase from './lib/connectToDb.js'
 
 const app = express()
-const port = 4000
-const dbURI = 'mongodb://localhost/contact-book-db'
 
 async function startServer() {
   try {
-    await mongoose.connect(dbURI, { useNewUrlPaser: true, useCreateIndex: true, useUnifiedTopology: true })
+    await connectToDatabase()
     console.log('Database has connected')
     app.listen(port, () => console.log(`Up and running on port ${port}`))
   } catch (err) {
@@ -17,3 +17,4 @@ async function startServer() {
 }
 
 startServer()
+
---
+++ models/contact.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const contactSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  phoneNumber: { type: Number, required: true, unique: true },
+  image: { type: String },
+})
+
+export default mongoose.model('Contact', contactSchema)
