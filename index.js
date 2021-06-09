f79acd433b4e96c6896ce52ba9cedfe94949f620
Max <maxmorgangiles@gmail.com>
2021-06-09 15:24:33 +0100

    get updated

--- index.js
+++ index.js
@@ -43,12 +43,11 @@ app.post('/contacts', async (req, res) => {
 app.get('/contacts/:id', async (req, res) => {
   const { id } = req.params
   try {
-    const contactToEdit = await Contact.findById(id)
-    if (!contactToEdit) throw new Error()
-    Object.assign(contactToEdit, req.body)
-    await contactToEdit.save()
-    return res.status(202).json(contactToEdit)
+    const contact = await Contact.findById(id)
+    if (!contact) throw new Error()
+    return res.status(200).json(contact)
   } catch (err) {
     console.log(err)
+    return res.status(404).json({ 'message': 'Not Found' })
   }
 })
