62591f14aebce4efceead44837b05c79f4bdfcfa
Max <maxmorgangiles@gmail.com>
2021-06-10 12:59:52 +0100

    error handler

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,59 +1,57 @@
 import Contact from '../models/contact.js'
+import { notFound } from '../lib/errorHandler.js'
 
-async function contactIndex(_req, res){
+async function contactIndex(_req, res, next){
   try {
     const contacts = await Contact.find()
     return res.status(200).json(contacts)
   } catch (err) {
-    console.log(err)
+    next(err)
   }
 }
 
-async function contactCreate (req, res) {
+async function contactCreate (req, res, next) {
   try {
     const newContact = await Contact.create(req.body)
     return res.status(201).json(newContact)
   } catch (err) {
-    console.log(err)
-    return res.status(422).json(err)
+    next(err)
   }
 }
 
-async function contactShow(req, res) {
+async function contactShow(req, res, next) {
   const { id } = req.params
   try {
     const contact = await Contact.findById(id)
-    if (!contact) throw new Error()
+    if (!contact) throw new Error(notFound)
     return res.status(200).json(contact)
   } catch (err) {
-    console.log(err)
-    return res.status(404).json({ 'message': 'Not Found' })
+    next(err)
   }
 }
 
-async function contactUpdate(req, res){
+async function contactUpdate(req, res, next){
   const { id } = req.params
   try {
     const contactToEdit = await Contact.findById(id)
-    if (!contactToEdit) throw new Error()
+    if (!contactToEdit) throw new Error(notFound)
     Object.assign(contactToEdit, req.body)
     await contactToEdit.save()
     return res.status(202).json(contactToEdit)
   } catch (err) {
-    console.log(err)
+    next(err)
   }
 }
 
-async function contactDelete(req, res) {
+async function contactDelete(req, res, next) {
   const { id } = req.params
   try {
     const contactToDelete = await Contact.findById(id)
-    if (!contactToDelete) throw new Error()
+    if (!contactToDelete) throw new Error(notFound)
     await contactToDelete.remove()
     return res.sendStatus(204)
   } catch (err) {
-    console.log(err)
-    return res.status(404).json({ 'message': 'Not Found' })
+    next(err)
   }
 }
 
--- index.js
+++ index.js
@@ -3,29 +3,38 @@ import { port } from './config/environment.js'
 import connectToDatabase from './lib/connectToDb.js'
 import logger from './lib/logger.js'
 import router from './config/router.js'
+import errorHandler from './lib/errorHandler.js'
 
 const app = express()
 
 async function startServer() {
   try {
     await connectToDatabase()
+    
     console.log('Database has connected')
+
+    app.use(express.json())
+
+    app.use(logger)
+
+    app.use(router)
+
+    app.use(errorHandler)
+
     app.listen(port, () => console.log(`Up and running on port ${port}`))
+
   } catch (err) {
+
     console.log('Something went wrong starting the 	app')
+
     console.log(err)
   }
 }
 
 startServer()
 
-app.use(express.json())
 
-app.use(logger)
-
-app.use(router)
-
-app.use((req, _res, next) => {
-  console.log(`🚨 Incoming Request: ${req.method} - ${req.url}`)
-  next()
-})
+// app.use((req, _res, next) => {
+//   console.log(`🚨 Incoming Request: ${req.method} - ${req.url}`)
+//   next()
+// })
