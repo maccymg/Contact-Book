f2e272f001569f454032efa97e71c2ca7e5dbb2b
Max <maxmorgangiles@gmail.com>
2021-06-14 16:09:16 +0100

    name change to Schema

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 const contactSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  secondName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phoneNumber: { type: String, required: true, unique: true },
   image: { type: String },
