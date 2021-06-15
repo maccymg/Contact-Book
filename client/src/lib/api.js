7a5fa5fae2adb9bda17d5ac1ffd58eed3d780c45
Max <maxmorgangiles@gmail.com>
2021-06-15 11:05:59 +0100

    lib api functions

---
+++ client/src/lib/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+
+const baseUrl = '/api'
+
+
+export function getAllContacts() {
+  return axios.get(`${baseUrl}/contacts`)
+}
+
+export function getSingleContact(id) {
+  return axios.get(`${baseUrl}/contacts/${id}`)
+}
+
+export function createContact(contactData) {
+  return axios.post(`${baseUrl}/contacts`, contactData)
+}
+
+export function deleteContact(id) {
+  return axios.delete(`${baseUrl}/contacts/${id}`)
+}
+
+export function editContact(editContactData, id) {
+  return axios.put(`${baseUrl}/contacts/${id}`, editContactData)
+}
--- client/src/styles.js
+++ client/src/styles.js
@@ -41,18 +41,12 @@ const useStyles = makeStyles((theme) => ({
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
       width: '20ch',
     },
-  },
-  addIcon: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    paddingLeft: '100px',
   },
 }))
 
